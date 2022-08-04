d384c0455b4fe52a89bb26f9b1b79f5e8c252955
Surenthar Rajamohan <surenthar.rajamohan@gmail.com>
2022-08-05 00:39:24 +0800

    Module 4 @author: Surenthar Rajamohan

---
+++ Module4_SurentharRajamohan/SpeakHello.js
@@ -0,0 +1,12 @@
+
+(function (window){
+var speakWord = "Hello";
+var helloSpeaker = {};
+
+helloSpeaker = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
